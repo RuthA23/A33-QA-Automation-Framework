ceee42e77b823afb069a151574b391a29ffcb5bc
RuthA23 <ruth.asghar@testpro.io>
2023-03-07 18:38:34 -0600

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -10,105 +10,111 @@ import java.time.Duration;
 public class LoginTests extends BaseTest {
 
     @Test
-    public static void LoginEmptyEmailPasswordTest () throws InterruptedException {
+<<<<<<<Updated upstream
+
+    public static void LoginEmptyEmailPasswordTest() throws InterruptedException {
         // Simple comment
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+=======
+        public static void LoginEmptyEmailPasswordTest () {
 
-        String url = "https://bbb.testpro.io/";
-        Thread.sleep(2000);
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        Thread.sleep(2000);
-        driver.quit();
-    }
+>>>>>>>Stashed changes
+            WebDriver driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-    @Test
-    public static void LoginInvalidEmail() {
-        //Login Not Existing Email:
-        //Precondition: Chrome browser should be opened DONE
-           // declare driver
-        //Step1. Open koel login page DONE
-           // tell driver to load koel page
-        //Step2. Enter Not Existing email
-           // find email field
-           // click into email field
-           // enter invalid email inside email field
-        //Step3. Enter Correct password
-           // find password field
-           // click into password field
-           // enter anything
-        //Step4. Click Login button
-           // find the login button
-           // click the login button
-        //Expected result: User should stay on login page
-           // assert the present of email field
+            String url = "https://apps.testpro.io/";
+            driver.get(url);
+            Assert.assertEquals(driver.getCurrentUrl(), url);
+            driver.quit();
+        }
+<<<<<<<Updated upstream
 
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        @Test
+        public static void LoginInvalidEmail () {
+            //Login Not Existing Email:
+            //Precondition: Chrome browser should be opened DONE
+            // declare driver
+            //Step1. Open koel login page DONE
+            // tell driver to load koel page
+            //Step2. Enter Not Existing email
+            // find email field
+            // click into email field
+            // enter invalid email inside email field
+            //Step3. Enter Correct password
+            // find password field
+            // click into password field
+            // enter anything
+            //Step4. Click Login button
+            // find the login button
+            // click the login button
+            //Expected result: User should stay on login page
+            // assert the present of email field
 
-        String url = "https://bbb.testpro.io/";
-        driver.get(url);
+            WebDriver driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        //Step2. Enter Not Existing email
-        // find email field
-        // click into email field
-        // enter invalid email inside email field
-        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
-        emailField.click();
-        emailField.sendKeys("cucaracha@class.com");
+            String url = "https://bbb.testpro.io/";
+            driver.get(url);
 
-        //Step3. Enter Correct password
-        // find password field
-        // click into password field
-        // enter anything
-        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
-        passwordField.click();
-        passwordField.sendKeys("ayCaramba");
+            //Step2. Enter Not Existing email
+            // find email field
+            // click into email field
+            // enter invalid email inside email field
+            WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+            emailField.click();
+            emailField.sendKeys("cucaracha@class.com");
 
-        //Step4. Click Login button
-        // find the login button
-        // click the login button
-        WebElement loginButton = driver.findElement(By.cssSelector("[type='submit']"));
-        loginButton.click();
+            //Step3. Enter Correct password
+            // find password field
+            // click into password field
+            // enter anything
+            WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+            passwordField.click();
+            passwordField.sendKeys("ayCaramba");
 
-        //Expected result: User should stay on login page
-        // assert the present of email field
-        Assert.assertTrue(emailField.isDisplayed());
-        driver.quit();
-    }
+            //Step4. Click Login button
+            // find the login button
+            // click the login button
+            WebElement loginButton = driver.findElement(By.cssSelector("[type='submit']"));
+            loginButton.click();
 
-    @Test
-    public static void LoginValidEmailPasswordTest() {
-        // Precondition: Chrome browser should be opened DONE
-        //Step1. Open koel login page DONE
-        //Step2. Enter Existing username
-        //Step3. Enter Correct password
-        //Step4. Click Login button
-        //Expected result: User should be directed to the home page
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            //Expected result: User should stay on login page
+            // assert the present of email field
+            Assert.assertTrue(emailField.isDisplayed());
+            driver.quit();
+        }
 
-        String url = "https://bbb.testpro.io/";
-        driver.get(url);
+        @Test
+        public static void LoginValidEmailPasswordTest () {
+            // Precondition: Chrome browser should be opened DONE
+            //Step1. Open koel login page DONE
+            //Step2. Enter Existing username
+            //Step3. Enter Correct password
+            //Step4. Click Login button
+            //Expected result: User should be directed to the home page
+            WebDriver driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
-        emailField.click();
-        emailField.sendKeys("demo@class.com");
+            String url = "https://bbb.testpro.io/";
+            driver.get(url);
 
-        //Step3. Enter Correct password
-        WebElement passwordField = driver.findElement(By.cssSelector("[type='password'"));
-        passwordField.click();
-        passwordField.sendKeys("te$t$tudent");
+            WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+            emailField.click();
+            emailField.sendKeys("demo@class.com");
 
-        //Step4. Click Login button
-        WebElement loginField = driver.findElement(By.cssSelector("[type='submit'"));
-        loginField.click();
+            //Step3. Enter Correct password
+            WebElement passwordField = driver.findElement(By.cssSelector("[type='password'"));
+            passwordField.click();
+            passwordField.sendKeys("te$t$tudent");
 
-        //Expected result: User should be directed to the home page
-        WebElement avatar = driver.findElement(By.className("avatar"));
-        Assert.assertTrue(avatar.isDisplayed());
+            //Step4. Click Login button
+            WebElement loginField = driver.findElement(By.cssSelector("[type='submit'"));
+            loginField.click();
 
-        driver.quit();
+            //Expected result: User should be directed to the home page
+            WebElement avatar = driver.findElement(By.className("avatar"));
+            Assert.assertTrue(avatar.isDisplayed());
+
+            driver.quit();
+        }
     }
 }
